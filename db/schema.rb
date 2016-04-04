37d25c21830d270b71d5b2a4df7f2caccac0c95a
gleamprodev <warnold233@gmail.com>
2016-04-04 05:52:12 +0200

    local RAILS_ENV=test bundle exec rake db:drop db:create db:migrate works

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,6 @@ ActiveRecord::Schema.define(version: 20160220222526) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at",    null: false
